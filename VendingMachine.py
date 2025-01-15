5141e4b99d7e6fba4b0d164ca9fda369ab3189c5
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2025-01-15 14:45:08 +0400

    second commit
    putting the products in a list/tuple named "menu"; making use of the list indexes to determine the code of the product; using if-else statements for assigning prices for each products; payment change system added

--- VendingMachine.py
+++ VendingMachine.py
@@ -1,36 +1,54 @@
-S01 = "cheesecake"
-S02 = "chips"
-S03 = "croissant"
-S04 = "donut"
-S05 = "bread"
-D06 = "strawberry latte"
-D07 = "matcha latte"
-D08 = "iced americano"
-D09 = "banana milk"
-D10 = "karak tea"
+# menu variables
+menu = ("cup noodles", "chips", "croissant", "orange juice", "iced americano", "karak tea")
+# menu_price = [AED10, AED5, AED2.50, AED3.50, AED5, AED1]
 
+# welcome message
 print("Hey there! Welcome to Hamartia Vending Machine!")
-print("MENU:\n"
-      "Snacks:\n"+
-      S01+"\n"+
-      S02+"\n"+
-      S03+"\n"+
-      S04+"\n"+
-      S05+"\n"+
-      "\nDrinks:\n"+ 
-      D06+"\n"+
-      D07+"\n"+
-      D08+"\n"+
-      D09+"\n"+
-      D10+"\n")
+# menu 
+print("""MENU:\n
+      Snacks:\t\t\t Drinks:
+      0 - cup noodles\t\t 3 - orange juice
+      1 - chips\t\t\t 4 - iced americano
+      2 - croissant\t\t 5 - karak tea\n""")
 
-order = input("What can I get for you today?\n")
+order_code = int(input("What can I get for you today? Please enter the order code.\n"))
+user_order = menu[order_code]
+user_bill = 0
+print(f"One {user_order} coming right up!")
 
-print("\nOne " + order + " coming right up!")
-
-input("Please enter the correct amount: ")
-if (input < "7"):
-    print("You paid the correct amount.")
-    
-if (input > "7"):
-    print("You paid the incorrect amount.")
+# while order_code >= 0 and order_code <= 5:    
+if order_code == 0:
+        user_bill += 10
+        print(f"The {user_order} cost AED 10")
+elif order_code == 2:
+        user_bill += 2.50
+        print(f"The {user_order} cost AED 2.50")
+elif order_code == 3:
+        user_bill += 3.50
+        print(f"The {user_order} cost AED 3.50")
+elif order_code == 5:
+        user_bill += 1
+        print(f"The {user_order} cost AED 1")
+elif order_code < 1 or order_code > 5:
+        print("You put the wrong number")
+else:
+        user_bill+= 5
+        print(f"The {user_order} cost AED 5")
+        
+add_item = (input("Would you like to purchase another item?\n"))
+if add_item.lower == "Yes":
+    print(input("Enter your code: "))
+elif add_item.lower == "No":
+    print("okie")
+        
+user_payment = float(input("Please enter the correct amount: "))
+if user_payment == user_bill:
+    print("You paid the correct amount.")    
+elif user_payment < user_bill:
+    input("You paid short. Please pay the correct amount: ")
+    if user_payment == user_bill:
+        print("You paid the correct amount.")
+else:
+    print("You paid more than the amount asked. Please wait for the change.")
+    change = user_payment - user_bill
+    print(f"Your change is AED {change:.2f}. Thank you for using our services!")
